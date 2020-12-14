a9fc2c71d6d295c08caa4ac4b17a75771d4491ff
Abdullah Raheel <Abdullaharaheel@gmail.com>
2020-12-14 07:27:15 +0500

    Made Grid, Mouse click working

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from queue import PriorityQueue
 
 Width = 800
 Win = pygame.display.set_mode((Width, Width))
-pygame.display.set_caption("A* Path Finding Algorithm")
+pygame.display.set_caption("Path-Finding Algorithm")
 
 
 red = (255, 0, 0)
@@ -51,28 +51,28 @@ class Node:
         return self.color == torquoise
 
     def reset(self):
-        return.color == white
+        return self.color == white
 
     #Assigning color for out nodes
     def create_visited(self):
-        return self.color = red
+        self.color = red
     
     def create_to_visit(self):
-        return self.color = green
+        self.color = green
 
     def create_barrier(self):
-        return self.color = black
+        self.color = black
 
     def create_start_node(self):
-        return self.color = orange
+        self.color = orange
 
     def create_end_node(self):
-        return self.color = torquoise
+        self.color = torquoise
     
     def create_path(self):
-        return self.color = purple
+        self.color = purple
 
-    def draw(self, Win):
+    def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
     
     def update_neighbours(sel, grid):
@@ -104,7 +104,7 @@ def create_grid(rows, width):
     for i in range(rows):
         grid.append([])
         for j in range(rows): #used rows here because A square grid is required
-            node = Node(i, j, gap, row, rows)
+            node = Node(i, j, gap, rows)
             grid[i].append(node)
 
     return grid
@@ -113,10 +113,10 @@ def draw_gridlines(win, rows, width):
     gap = width // rows
     for i in range(rows):
         pygame.draw.line(win, grey, (0, i * gap), (width, i * gap))
-        for i in range(rows):
+        for j in range(rows):
             pygame.draw.line(win, grey, (j * gap, 0), (j * gap, width))
 
-def draw(win, grid, rows, width)
+def draw(win, grid, rows, width):
     win.fill(white)
     
     for row in grid:
@@ -128,8 +128,8 @@ def draw(win, grid, rows, width)
 
 def mouse_clicked_position(position, rows, width):
     gap = width // rows
-    x = position
-    y = position
+    x = position[1]
+    y = position[0]
 
     col = x // gap
     row = y // gap
@@ -139,5 +139,38 @@ def mouse_clicked_position(position, rows, width):
 def main(win, width):
     rows = 50
     grid = create_grid(rows, width)
+
+    start_position = None
+    end_position = None
+
+    run = True
+    Started = False
+    while run:
+        draw(win, grid, rows, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if Started:
+                continue
+            
+            if pygame.mouse.get_pressed()[0]:
+                position = pygame.mouse.get_pos()
+                row, col = mouse_clicked_position(position, rows, width)
+                node = grid[row][col]
+                if not start_position:
+                    start_position = node
+                    start_position.create_start_node()
+                elif not end_position:
+                    end_position = node
+                    end_position.create_end_node()
+                else:
+                    node.create_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:
+                pass
     
-    
+    pygame.quit()
+
+
+main(Win, Width)
